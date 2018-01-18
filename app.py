7ad3a3c6dba5ac7a594400003446e9a375ed5171
Thomas Chadrycki <thomaschadrycki@dyn-160-39-179-224.dyn.columbia.edu>
2018-01-17 21:44:44 -0500

    Updated files with tasks
    Finished doing the technical challange for ADI labs.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/tc2843', methods=['GET'])
+def main():
+    return render_template('profile.html')
+
 
 if __name__ == '__main__':
     app.run()
